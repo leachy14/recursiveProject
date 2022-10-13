209b0fb42462d2d6857097f555f18411263adb6d
Christopher Leach <leach@hey.com>
2022-10-13 10:28:24 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    int x;
+    int y;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int problemOne() {
+
+    }
+}
