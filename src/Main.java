596cd1957c8c09094a30ac74170a5bed18a83087
Christopher Leach <leach@hey.com>
2022-10-13 17:50:08 -0400

    added problem three and four

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Main {
 
     int x;
@@ -7,6 +8,10 @@ public class Main {
         System.out.println(problemOne(8,12));
         //problem two
         System.out.println(problemTwo(problemTwo(problemTwo(problemTwo(problemTwo(10))))));
+        //problem three
+        System.out.println(problemThree(30,12));
+        //problem four
+        System.out.println(32);
 
     }
 
@@ -32,4 +37,24 @@ public class Main {
             return x + 5;
         }
     } // end problemTwo
+
+    // problem three
+    public static int problemThree(int x, int y) {
+        if (x > y) {
+            return Math.max(problemThree(x - 4, y + 3), problemThree(y,x));
+            } else {
+            return x * y;
+        }
+    } // end problemThree
+
+    public static int problemFour(int x) {
+        if ((x % 2) == 0) {
+            return problemFour(x/2 - 1) + 3;
+        } else if (x > 0) {
+            return 2 * problemFour(x - 3) - 5;
+        } else {
+            return x * x - 3;
+        }
+    } // end problemFour
+
 }
