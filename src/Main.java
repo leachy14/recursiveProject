d467f05cdc804b6de05d4a5b166c972b8b05d817
Christopher Leach <leach@hey.com>
2022-10-13 17:18:59 -0400

    added problem one

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,21 @@ public class Main {
     int x;
     int y;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(problemOne(8,12));
     }
 
-    public int problemOne() {
+    //recursive method 1
+    public static int problemOne(int x, int y) {
+        if (x < y) {
+            return problemOne(x + 1, y - 2) + 3;
+        } else if (x > y) {
+            return problemOne(y - 1, x) - 1;
+        } else {
+            return x * x + y * y;
+        }
 
-    }
+    } //end problemOne
+
+    //recursive method 2
+
 }
