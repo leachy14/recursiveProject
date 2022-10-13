8c2a54e0e37afbbf48f054c1057366d6f3c27fe0
Christopher Leach <leach@hey.com>
2022-10-13 17:30:11 -0400

    added problem two

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     int x;
     int y;
     public static void main(String[] args) {
+        //problem one
         System.out.println(problemOne(8,12));
+        //problem two
+        System.out.println(problemTwo(problemTwo(problemTwo(problemTwo(problemTwo(10))))));
+
     }
 
     //recursive method 1
@@ -19,5 +23,13 @@ public class Main {
     } //end problemOne
 
     //recursive method 2
-
+    public static int problemTwo(int x) {
+        if (x > 7) {
+            return problemTwo(x - 5) - 3;
+        } else if (x > 3 && x < 7) {
+            return problemTwo(x + 2) + 2;
+        } else {
+            return x + 5;
+        }
+    } // end problemTwo
 }
